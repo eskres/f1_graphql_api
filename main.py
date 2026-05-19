28a7205f5f6e05443d24d1927bcddae0f0f43465
Einar <einar.skreslett@gmail.com>
2026-05-19 22:29:25 +0100

    Replace hello world with GraphQL router mounted from resolvers module.

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import strawberry
 from fastapi import FastAPI
-from strawberry.fastapi import GraphQLRouter
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "Hello World!"
-
-schema = strawberry.Schema(query=Query)
-
-graphql_app = GraphQLRouter(schema)
+from app.resolvers import graphql_app
 
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
