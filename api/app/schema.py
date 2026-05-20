327ccfc46aee8848c3c83930b5c3c695319b8325
Einar <einar.skreslett@gmail.com>
2026-05-20 21:22:05 +0100

    Add SeasonType with total_pit_time and update SeasonRaceType with total_pit_time field

--- api/app/schema.py
+++ api/app/schema.py
@@ -17,3 +17,28 @@ class PitStopType:
     time: Optional[str]
     time_millis: Optional[int]
     constructor_id: Optional[str]
+
+@strawberry.type
+class SeasonRaceType:
+    id: int
+    official_name: str
+    round: int
+
+@strawberry.type
+class DriverPitStopType:
+    driver_id: str
+    constructor_id: str
+    total_pit_time: int
+    stop_count: int
+
+@strawberry.type
+class SeasonType:
+    year: int
+    total_pit_time: int
+
+@strawberry.type
+class SeasonRaceType:
+    id: int
+    official_name: str
+    round: int
+    total_pit_time: int
